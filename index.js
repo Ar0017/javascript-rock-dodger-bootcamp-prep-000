bfb37e1e2cc8306b583258da6631b147d20d04c7
Arinze <arinzedile@yahoo.com>
2018-01-03 05:23:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function createRock(x) {
   rock.style.left = `${x}px`
 
   // Hmmm, why would we have used `var` here?
-  var top = rock.style.top = top
+  var top = rock.style.top = 0
 
   /**
    * Now that we have a rock, we'll need to append
